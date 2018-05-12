d37511d1e030dfa85fd04501f3a1b49a573f2022
Dessagn <temesgen.dessalegn@gmail.com>
2018-05-12 16:27:26 +0300

    serever.js modified
    connection to mongodb established
    
    Story: B-00000

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+//create express app
 const app = express();
+//configure db source
 const db = require('./config/keys').mongoUri
+// Configure port
 const port = process.env.PORT || 5000
 
+// connect to mongodb using mongoose
 mongoose
   .connect(db)
   .then(() => console.log('mongodb connected'))
